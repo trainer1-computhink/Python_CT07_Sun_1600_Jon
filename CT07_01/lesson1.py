bb184f362e2e140058da89e4636e6f9fb0f63ef0
trainer1-computhink <trainer1@computhink.com.sg>
2026-01-19 18:21:52 +0000

    updates

--- CT07_01/lesson1.py
+++ CT07_01/lesson1.py
@@ -1,2 +1,18 @@
 print("Hello from lesson 1")
 
+## Recap 1: Pseudocode (Recycling logic)
+# Task: Write down the steps on how to solve the problem below
+
+# Design pseudocode for a recycling robot that sorts items
+# into bins for glass, plastic, and paper. The robot should
+# check each item's material and place it in the correct bin.
+
+# create bin for glass, plastic and paper, general waste
+# if items is glass
+#    put into the glass bin
+# if item is plastics
+#    put into the plastic bin
+# if item is paper 
+#    put into the paper bin
+# else 
+# put into the general wastee
--- CT07_02/lesson2.py
+++ CT07_02/lesson2.py
@@ -1,2 +1,17 @@
 print("Hello from lesson 2")
 
+# # count from 100 to 1
+# for count in range(100,0,-1):
+#     print(count)
+
+# count = 1
+# for i in range(5):
+#     for j in range(2):
+#         print(f"bababa " + str(count) +" times")
+#         count += 1
+
+# for i in range(20):
+#     if i%7 == 0:
+#         print(i)
+
+print("toppings " + "abc")
