296e06142b989ce705afaaa22907b98209a3ca8b
trainer1-computhink <trainer1@computhink.com.sg>
2026-02-28 08:50:20 +0000

    Pending changes exported from your codespace

--- CT07_05/lesson5.py
+++ CT07_05/lesson5.py
@@ -1,23 +1,86 @@
-print("Hello from lesson 5")
+# print("Hello from lesson 5")
 import random
-num_list = []
-for i in range(10):
-    ran_num = random.randint(0,10)
-    if not ran_num in num_list:
-        num_list.append(ran_num)
-print(num_list)
-# print(len(num_list))
+# # num_list = []
+# # for i in range(10):
+# #     ran_num = random.randint(0,10)
+# #     if not ran_num in num_list:
+# #         num_list.append(ran_num)
+# # print(num_list)
+# # print(len(num_list))
 
-# while loop ? 
-# when to break out of the loop?
-# when you your list length == 100
+# # while loop ? 
+# # when to break out of the loop?
+# # when you your list length == 100
 
-# for i in range(100):
-#     num_list .append(random.randint(65,100))
+# # for i in range(100):
+# #     num_list .append(random.randint(65,100))
     
-# print(num_list)
+# # print(num_list)
 # namelist = ["Olivia", "Liam", "Emma", "Noah", "Ava", "Ethan",
 # "Sophia", "Lucas", "Mia", "Aiden"]
 
-# heightlist = [160, 165, 158, 170, 162, 168, 159, 1,72, 164, 166]
-print(num_list.index(3))
+# heightlist = [160, 165, 158, 170, 162, 168, 159, 172, 164, 166]
+# print(sum(heightlist))
+# print(len(heightlist))
+# print(sum(heightlist)/len(heightlist))
+# print(min(heightlist))
+# print(max(heightlist))
+
+# min_height = min(heightlist)
+# min_height_ind = heightlist.index(min_height)
+# print(min_height_ind)
+# #by knowing the namelist and the index of the shortest, how do we know the person name?
+# min_height_name = namelist[min_height_ind]
+# print(min_height_name)
+
+
+# print(num_list.index(3))
+
+## Task 5: Pokemon, I choose you!
+# Task: You are given 2 lists,
+# **pokemons** contains a list of pokemons
+# **powers** contains a list of the corresponding pokemon's
+#            powers
+
+# 1. Choose 2 random pokemons from the list
+# 2. Compare the powers of the 2 pokemons
+# 3. Calculate who is the winner of the fight between these 2
+#    pokemons
+#    (pokemon with the higher power will always win)
+
+# Sample data (Copy + paste the below code):
+pokemons = [
+    "Pikachu", "Charizard", "Bulbasaur", "Squirtle",
+    "Jigglypuff", "Meowth", "Psyduck", "Eevee", "Snorlax",
+    "Mewtwo", "Lapras", "Gengar", "Dragonite", "Machamp",
+    "Arcanine", "Alakazam", "Gyarados", "Vaporeon", "Scyther",
+    "Electabuzz"
+]
+
+pokemon1 = random.choice(pokemons)
+pokemon2 = random.choice(pokemons)
+print(pokemon1, pokemon2)
+
+pokemon1_index = pokemons.index(pokemon1)
+pokemon2_index = pokemons.index(pokemon2)
+print(pokemon1_index, pokemon2_index)
+
+powers = [
+    55, 84, 49, 48, 45,
+    45, 52, 55, 110, 110,
+    85, 65, 134, 130, 110,
+    50, 125, 65, 110, 83
+]
+
+pokemon1_powers = powers[pokemon1_index]
+pokemon2_powers = powers[pokemon2_index]
+print(pokemon1_powers, pokemon2_powers)
+
+if pokemon1_powers > pokemon2_powers:
+    print(pokemon1 + " wins.")
+elif pokemon1_powers < pokemon2_powers:
+    print(pokemon2 + " wins")
+else:
+    print("It is a tie between " + pokemon1 + " and " + pokemon2)
+
+# Hint: import the random library and use random.choice(listname)
--- CT07_06/lesson6.py
+++ CT07_06/lesson6.py
@@ -1,2 +1,77 @@
 print("Hello from lesson 6")
 
+# # Lesson 6 - 2-dimensional list
+
+# ## Recap 1: List of 100 unique numbers
+# **Recap 1a**:
+# You are preparing for an upcoming lucky draw session at your
+# school. However, there must be no repeating winning numbers.
+
+# Task: Create a program to create 100 random unique numbers in
+# a list
+# 1. Use a loop to add 100 random numbers into your list.
+# 2. Each number added range between 1 to 1000
+# 3. Ensure that all the numbers are unique
+
+# **Recap 1b**:
+# You have been asked to provide some statistics based on the
+# list of numbers generated.
+
+# 1. Using max(), find the highest number from the list
+# 2. Using min(), find the lowest number from the list
+# 3. Using sum() and len(), find the average from the list
+# 4. By importing the 'random' library and using random.choice(),
+#    print out a random number from the list.
+# 5. Using index(), print out the index of the printed random
+#    number.
+
+
+# list_nums = [2, 3,4 ,5, 6, 7]
+# print(list_nums)
+# for num in list_nums:
+#     print(num)
+
+
+# students = [
+#     ["Olivia", "F"], ["Noah", "M"], ["Emma", "F"],
+#     ["Liam", "M"], ["Ava", "F"], ["Ethan", "M"],
+#     ["Sophia", "F"], ["Lucas", "M"], ["Mia", "F"],
+#     ["Aiden", "M"], ["Isabella", "F"], ["Jackson", "M"],
+#     ["Amelia", "F"], ["Logan", "M"], ["Lily", "F"]
+# ]
+
+# boys = []
+# girls = []
+
+# for student in students:
+#     name, gender = student
+#     if gender == "F":
+#         girls.append(name)
+#     else:
+#         boys.append(name)
+# print(boys)
+# print(girls)
+# print(len(boys))
+# print(len(girls))
+
+
+a1 = [
+    [1 , 2, 3],
+    [4, 5, 6],
+    [7, 8 ,9]
+]
+
+a2 = [
+    [7 , 9, 3],
+    [7, 5, 4],
+    [1, 8 ,4]
+]
+
+# find a3 if i want to add for example [1,2,3] and [7,9,3] = [1+7, 2+9, 3+3]
+# i want you to add each element at the respective to one another for a1 and a2
+a3 = []
+
+for i in range(len(a1)):
+    for j in range(len(a1)):
+        sum = a1[i][j] + a2[i][j]
+        a3.append(a3[i].append(sum))
